dc6c04c576858f81d17f6575ddf333b11625e46a
Andrew Diles <A_Diles@hotmail.com>
2020-08-06 13:12:49 -0400

    attemping to serve all React's endpoints via express6

--- server/index.js
+++ server/index.js
@@ -127,19 +127,19 @@ App
   //   gameInfo:
   // }
 
-  // .use(function(req, res, next) {
-  //   res.sendFile(path.join(__dirname, '../public', 'app.html'));
-  // })
+ 
 
-  router.get(['/app', '/app/*'], function(req, res, next) {
-    console.log('hitting end point #1')
-    res.sendFile(path.join(__dirname, '../public', 'index.html'));
-  })
 
   .use(['/app', '/app/*'], function(req, res, next) {
-    console.log('hitting end point #2')
+    console.log('hitting end point #1');
     res.sendFile(path.join(__dirname, '../public', 'index.html'));
   })
+
+  .use(function(req, res, next) {
+    console.log('hitting end point #2');
+    res.sendFile(path.join(__dirname, '../public', 'app.html'));
+  })
+
 
   .listen(PORT, () => console.info(`Listening on port ${PORT}`));
 
