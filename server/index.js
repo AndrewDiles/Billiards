57d608ed82c2d5ae9bb37274ae0c80a87adff4f2
Andrew Diles <A_Diles@hotmail.com>
2020-08-08 22:44:54 -0400

    adding serve static

--- server/index.js
+++ server/index.js
@@ -52,7 +52,7 @@ App
   // .use('/view-lobby', express.static("../build/"))
   // .use('/', express.static("../build/"))
 
-  // .use('../src/assets/*', express.static('../build'))
+  .use('../src/assets/*', express.static('../build'))
   .get('/', function(req, res, next) {
     res.sendFile(path.join(__dirname, '../build/index.html'))
   })
